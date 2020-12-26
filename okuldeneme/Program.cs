20c2304373a8f3008629de45ecd8c90206f2b0f8
Mehmet Çetin <54488134+Melocet@users.noreply.github.com>
2020-12-26 16:46:45 +0300

    Add files via upload
    upload

---
+++ okuldeneme/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace okuldeneme
+{
+    class Program
+    {
+        // Erişim Şekli - Dönüş tipi - metotadı - parametre listesi
+        static void Main(string[] args)
+        {
+            string st = "Deneme!";
+            int a = 78;
+            Console.WriteLine("Merhaba"); // Metot İçerisinde işlenen işlemler
+            Console.WriteLine("002!");
+            Console.WriteLine("2" + "16");
+        }
+    }
+}
