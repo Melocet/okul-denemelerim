a98c466f03d4100a6aa38a0299c8b7c28bbbb907
Mehmet Çetin <54488134+Melocet@users.noreply.github.com>
2020-12-27 12:05:49 +0300

    Add files via upload

--- okuldeneme/Program.cs
+++ okuldeneme/Program.cs
@@ -5,13 +5,21 @@ namespace okuldeneme
     class Program
     {
         // Erişim Şekli - Dönüş tipi - metotadı - parametre listesi
-        static void Main(string[] args)
+        static void Yazilim()
         {
-            string st = "Deneme!";
-            int a = 78;
             Console.WriteLine("Merhaba"); // Metot İçerisinde işlenen işlemler
             Console.WriteLine("002!");
             Console.WriteLine("2" + "16");
         }
-    }
+        static void Main(string[] args)
+        {
+            Yazilim();  // Metodun çağrırma komudu
+            Yazilim();
+            Yazilim(); // Tekrarlama
+            Yazilim();
+            Yazilim();
+            Yazilim();
+            Console.ReadKey();
+      }
+   }
 }
