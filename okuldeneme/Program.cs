6f46e894db8d6da9f4a44c03c2a48a13823d5842
Mehmet <54488134+Melocet@users.noreply.github.com>
2021-02-22 10:36:38 +0300

    Update Program.cs
    -update

--- okuldeneme/Program.cs
+++ okuldeneme/Program.cs
@@ -1,25 +1,81 @@
-﻿using System;
+using System;
 
-namespace okuldeneme
+namespace melocet
 {
     class Program
     {
-        // Erişim Şekli - Dönüş tipi - metotadı - parametre listesi
-        static void Yazilim()
-        {
-            Console.WriteLine("Merhaba"); // Metot İçerisinde işlenen işlemler
-            Console.WriteLine("002!");
-            Console.WriteLine("2" + "16");
-        }
         static void Main(string[] args)
         {
-            Yazilim();  // Metodun çağrırma komudu
-            Yazilim();
-            Yazilim(); // Tekrarlama
-            Yazilim();
-            Yazilim();
-            Yazilim();
-            Console.ReadKey();
-      }
-   }
+            // Basitçe
+
+            //int sayi1=17;
+            //int sayi2=22;
+
+            //int toplam;
+
+            //toplam = sayi1 + sayi2;
+
+            //Console.Write(toplam);
+
+            /////////////////////////////////////////////////////////
+
+            //string ad = "mehmet ";
+            //string soyad = "melocet";
+
+            //string isim = ad + soyad;
+
+            //Console.WriteLine(isim);
+
+            //Console.Read();
+
+            ///////////////////////////////////////////////////////
+
+            // String test btw
+
+            //string sayi1 = "17";
+            //string sayi2 = "22";
+
+            //string toplam;
+
+            //toplam = sayi1 + sayi2;
+
+            //Console.Write(toplam);
+
+            //Console.Read();
+
+            ////////////////////////////////////////////////////////
+
+            // diğer şey
+
+            //bool sonuc;
+            //sonuc = true;
+            //sonuc = false;
+
+            //Console.WriteLine(sonuc);
+
+            //Console.Read();
+
+            //////////////////////////////////////////////////////
+
+            int[] sayilar = { 10, 20, 30, 40, 55, 60, };
+
+            Console.WriteLine("0.Hacı : {0}", sayilar[0]);
+            Console.WriteLine("1.Hacı : {0}", sayilar[1]);
+            Console.WriteLine("2.Hacı : {0}", sayilar[2]);
+            Console.WriteLine("3.Hacı : {0}", sayilar[3]);
+            Console.WriteLine("4.Hacı : {0}", sayilar[4]);
+
+
+            string[] isimler = { "mehmet", "melocet", "atatürk" };
+
+            Console.WriteLine();
+            Console.WriteLine("1.Hacı : {0}", isimler[0]);
+            Console.WriteLine("2.Hacı : {0}", isimler[0]);
+            Console.WriteLine("3.Hacı : {0}", isimler[0]);
+
+
+
+            Console.Read();
+        }
+    }
 }
